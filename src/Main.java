235cc35dbb529b01cff15a9427e8dc542798f074
Ivan Cruz <25679384+UntalivanCruz@users.noreply.github.com>
2025-07-28 20:58:57 -0600

    fix: se reparo el ingreso del nombre y se refactorizo el codigo

--- src/Main.java
+++ src/Main.java
@@ -7,37 +7,41 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese el número de alumnos: ");
         int numAlumnos = scanner.nextInt();
+        scanner.nextLine(); // Limpiar el buffer del scanner
 
-        scanner.nextLine(); // Consumir el salto de línea
         //Arreglos multidimensionales para nombres y notas
         String[] nombres = new String[numAlumnos];
         double[] notas = new double[numAlumnos];
-
+        double sumaNotas = 0;
+        //INPUT
         for (int i = 0; i < numAlumnos; i++) {
             System.out.print("Ingrese el nombre del alumno " + (i + 1) + ": ");
             nombres[i] = scanner.nextLine();
 
             double nota;
+            boolean esNotaIncorrecta;
             do {
                 System.out.print("Ingrese la nota del alumno " + (i + 1) + " (entre 0 y 100): ");
                 nota = scanner.nextDouble();
-                if (nota < 0 || nota > 100) {
+                scanner.nextLine(); // Limpiar el buffer del scanner
+
+                esNotaIncorrecta = nota < 0 || nota > 100; // true o false
+                if (esNotaIncorrecta) {
                     System.out.println("Nota inválida. Debe estar entre 0 y 100.");
+                    continue;  //break and continue the loop
                 }
                 notas[i] = nota;
-            } while (nota < 0 || nota > 100);
+                sumaNotas += nota;
+            } while (esNotaIncorrecta);
         }
-
+        //PROCESO
         //Calcular promedio
-        double sumaNotas = 0;
-        for (double nota : notas) {
-            sumaNotas += nota;
-        }
-
         double promedio = sumaNotas / numAlumnos;
         System.out.println("El promedio de las notas es: " + promedio);
         //Mostrar alumnos aprobados y reprobados
         System.out.println("Alumnos aprobados:");
+
+        //OUTPUT
         for (int i = 0; i < numAlumnos; i++) {
             if (notas[i] >= 60) {
                 System.out.println(nombres[i] + " con nota: " + notas[i]);
